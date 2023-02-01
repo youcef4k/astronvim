f1564e29af88d90129db4e32e6b47a2c2ecd4a64
youcef4k <youssouf.sameut_bouhaik@hotmail.com>
2023-02-01 11:07:51 +0100

    Remove updater settings to use default

--- init.lua
+++ init.lua
@@ -6,25 +6,6 @@
 -- where a value with no key simply has an implicit numeric key
 local config = {
 
-  -- Configure AstroNvim updates
-  updater = {
-    remote = "origin", -- remote to use
-    channel = "nightly", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "main", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
-    show_changelog = true, -- show the changelog after performing an update
-    auto_reload = false, -- automatically reload and sync packer after a successful update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    -- remotes = { -- easily add new remotes to track
-    --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-    --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-    --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    -- },
-  },
-
   -- Set colorscheme to use
   colorscheme = "gruvbox",
   -- colorscheme = "default_theme",
@@ -208,16 +189,16 @@ local config = {
   -- Configure plugins
   plugins = {
     init = {
-    { 'michaelb/sniprun', run = 'bash ./install.sh',
-    config = function()
-      require('sniprun').setup{
-        display = {"NvimNotify"},
-        display_options = {
-          notification_timeout = 4   -- timeout for nvim_notify output
-        },
+      { 'michaelb/sniprun', run = 'bash ./install.sh',
+        config = function()
+          require('sniprun').setup {
+            display = { "NvimNotify" },
+            display_options = {
+              notification_timeout = 4 -- timeout for nvim_notify output
+            },
+          }
+        end,
       }
-    end,
-    }
       -- You can disable default plugins as follows:
       -- ["goolord/alpha-nvim"] = { disable = true },
 
